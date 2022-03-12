28139c98ef2c59efddaf6cdf0d7c52cd262c05a2
Anant <amishra1577@gmail.com>
2022-03-12 19:46:12 +0530

    Modified search.py

--- search.py
+++ search.py
@@ -8,8 +8,8 @@ dist = 15
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, help = "Image to search")
-ap.add_argument("-t", "--vptree", default = "vptree.pickle", help = "Path to generated VPtree")
-ap.add_argument("-a", "--hashes", default = "hashes.pickle", help = "Path of generated hashes")
+ap.add_argument("-t", "--vptree", default = "Resources\\vptree.pickle", help = "Path to generated VPtree")
+ap.add_argument("-a", "--hashes", default = "Resources\\hashes.pickle", help = "Path of generated hashes")
 args = vars(ap.parse_args())
 
 # Loading image and calculating its hash value
@@ -40,5 +40,6 @@ cv2.imshow("Input",queryimg)
 for (index,hashval) in res:
     for respath in hashes[hashval]:
         resimg = cv2.imread(respath)
-        cv2.imshow("Output",cv2.resize(resimg,(240,240)))
+        print(resimg)
+        cv2.imshow("Output",resimg)
         cv2.waitKey(0)
